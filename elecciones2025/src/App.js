0dbd7010186469a3a7475ac746e81ba50dd609bf
JuanSosaDias <juan.sosadias@correo.ucu.edu.uy>
2025-05-05 20:34:04 -0300

    Project creation with React

---
+++ elecciones2025/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ELECCIONES 2025
+        </p><br></br>
+        <p>
+          A QUIEN VAS A VOTAR?
+        </p><br></br>
+        <input></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
